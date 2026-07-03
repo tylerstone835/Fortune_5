d447c4c873d6d3df826a31d1797c1618a60a5a45
tst <tylerstone835@gmail.com>
2026-07-03 13:52:46 -0600

    Create plot_candle() chart style

--- Utils/chart_utils.py
+++ Utils/chart_utils.py
@@ -41,3 +41,39 @@ def plot_ohlc(
     axes.tick_params(axis='y', direction='out', length=1.5, labelcolor=(.9, .9, .9, .65), labelsize='xx-small', color=(0,0,0,0))
     axes.set_xbound(lower=-.5, upper=len(df) - .5)
     axes.set_frame_on(False)
+
+
+def plot_candle(
+    axes: plt.axes,
+    df: pd.DataFrame,
+    xticks: pd.Series = pd.Series(),
+) -> None:
+    """
+    Plot OHLC chart on child axes. Requires OHLC data
+
+    :param axes: Child axes on matplotlib.pyplot.figure
+    :param df: Source pd.DataFrame. Requires OHLC.
+    :param xticks: Add a custom series of date xticks, else blank.
+    """
+
+    required_columns_set = {'date', 'open', 'high', 'low', 'close'}
+
+    if not required_columns_set <= set(df.columns):
+        raise ValueError('Missing necessary data to construct chart')
+
+    for row in df.itertuples(index=False):
+
+        color = _GREEN if row.close >= row.open else _RED
+
+        axes.plot([row.date, row.date], [row.open, row.close], linestyle='-', color=color, linewidth=3)
+        axes.plot([row.date, row.date], [row.low, row.high], linestyle='-', color=color, linewidth=.5)
+
+
+        # Style Settings
+        axes.grid(visible=True, linestyle=':', alpha=_GRID_ALPHA, zorder=0)
+        axes.set_xticks(xticks)
+        axes.set_xticklabels([fdate.date().strftime('%b-%y') for fdate in xticks.astype('datetime64[ns]')])
+        axes.tick_params(axis='x', direction='in', length=0)
+        axes.tick_params(axis='y', direction='out', length=1.5, labelcolor=(.9, .9, .9, .65), labelsize='xx-small', color=(0, 0, 0, 0))
+        axes.set_xbound(lower=-.5, upper=len(df) - .5)
+        axes.set_frame_on(False)
