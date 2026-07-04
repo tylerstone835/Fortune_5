860f0e39bed66c871c7cd1077a197689d1124966
tst <tylerstone835@gmail.com>
2026-07-03 18:37:59 -0600

    Change green day logic to previous close comp.

--- Utils/df_utils.py
+++ Utils/df_utils.py
@@ -4,11 +4,19 @@ def is_green_day(
     df: pd.DataFrame,
 ) -> bool:
     """
-    Determine if latest day in stock df was green.
+    Determine if latest day closed higher.
     :param df: df containing stock data for eval.
     :return: boolean expression confirming if latest stock day was green.
     """
 
-    latest_row = df.loc[df['date'] == df['date'].max(), ['open', 'close']].to_dict(orient='list')
+    is_green_df = (
+        df
+        .sort_values(by='date', ascending=False)
+        .reset_index(drop=True)
+        .head(2)
+    )
 
-    return latest_row['close'][0] >= latest_row['open'][0]
+    if len(is_green_df) == 2:
+        return is_green_df.loc[0, 'close'] >= is_green_df.loc[1, 'close']
+
+    return True
