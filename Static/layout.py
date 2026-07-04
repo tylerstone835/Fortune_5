ba9b54ee5063fb8027f29e254ad513e87cfecd47
tst <tylerstone835@gmail.com>
2026-07-03 20:23:53 -0600

    Layout Kwargs

---
+++ Static/layout.py
@@ -0,0 +1,19 @@
+layout_kwargs: dict = {
+    1: {
+        'figsize': (10, 4.9)
+    },
+    2: {
+        'figsize': (10, 4.9),
+        'nrows': 2,
+        'ncols': 1,
+        'sharex': True,
+        'height_ratios': [.85, .15]
+    },
+    3: {
+        'figsize': (10, 4.9),
+        'nrows': 3,
+        'ncols': 1,
+        'sharex': True,
+        'height_ratios': [.73, .07, .20]
+    }
+}
