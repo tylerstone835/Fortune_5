d31c4efc3fdd6f045ceed9e2042a946e206f3cad
tst <tylerstone835@gmail.com>
2026-07-04 15:16:22 -0600

    Adjust figheight to make room for viz selection.

--- Static/layout.py
+++ Static/layout.py
@@ -1,16 +1,16 @@
 layout_kwargs: dict = {
     1: {
-        'figsize': (10, 4.9)
+        'figsize': (10, 4.5)
     },
     2: {
-        'figsize': (10, 4.9),
+        'figsize': (10, 4.5),
         'nrows': 2,
         'ncols': 1,
         'sharex': True,
         'height_ratios': [.85, .15]
     },
     3: {
-        'figsize': (10, 4.9),
+        'figsize': (10, 4.5),
         'nrows': 3,
         'ncols': 1,
         'sharex': True,
