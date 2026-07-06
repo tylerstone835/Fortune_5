13afd5eeb9aa5a606d81ea9588cc3983dc59d4a6
tst <tylerstone835@gmail.com>
2026-07-05 21:48:17 -0600

    adjust layout ratios

--- Static/layout.py
+++ Static/layout.py
@@ -1,16 +1,16 @@
 layout_kwargs: dict = {
     1: {
-        'figsize': (10, 4.5)
+        'figsize': (10, 4.4)
     },
     2: {
-        'figsize': (10, 4.5),
+        'figsize': (10, 4.4),
         'nrows': 2,
         'ncols': 1,
         'sharex': True,
         'height_ratios': [.85, .15]
     },
     3: {
-        'figsize': (10, 4.5),
+        'figsize': (10, 4.4),
         'nrows': 3,
         'ncols': 1,
         'sharex': True,
