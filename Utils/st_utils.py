d93d8742abf7f777b2da00ca9c2928a352f50388
tst <tylerstone835@gmail.com>
2026-07-06 01:48:06 -0600

    create draw_body()

--- Utils/st_utils.py
+++ Utils/st_utils.py
@@ -4,7 +4,8 @@ from pandas import DataFrame
 from pandas.io.parsers import TextFileReader
 
 from Static.company import company_dict
-from Utils.df_utils import customize_dataframe
+from Utils.df_utils import customize_dataframe, calculate_close_delta
+from Utils.chart_utils import draw_dashboard_figure
 
 
 @st.cache_data(scope='session')
@@ -134,3 +135,50 @@ def draw_footer() -> None:
             body='[Visit GitHub Repository](https://github.com/tylerstone835/Fortune_5)',
             text_alignment='right'
         )
+
+
+def draw_body(
+    sb_config: dict
+) -> None:
+    """
+    Draw body of dashboard with data selection and input elements from sidebar.
+
+    :param sb_config:
+    """
+
+    # Body layout
+    st.title('Fortune 5 - Price Action Analysis', text_alignment='center')
+    with st.container(border=True, horizontal_alignment='center'):
+
+        with st.container(horizontal=True):
+
+            with st.container(horizontal_alignment='left', vertical_alignment='top'):
+                st.metric(
+                    label='Close Price',
+                    value=sb_config['data'].loc[sb_config['data'].index.max(), 'close'],
+                    delta=calculate_close_delta(sb_config['data']),
+                    label_visibility='collapsed'
+                )
+
+            with st.container(horizontal_alignment='right'):
+                viz_output = st.segmented_control(
+                    label='**Visualization Type**',
+                    label_visibility='collapsed',
+                    options=['Chart', 'DataFrame'],
+                    selection_mode='single',
+                    default='Chart',
+                    required=True
+                )
+
+        if viz_output == 'Chart':
+            st.pyplot(
+                fig=draw_dashboard_figure(sb_config),
+                width='content'
+            )
+
+        elif viz_output == 'DataFrame':
+            st.dataframe(
+                sb_config['data'].sort_values(by='date', ascending=False),
+                height='content',
+                hide_index=True
+            )
