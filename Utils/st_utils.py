6969af6e048da18e7a539fa9585fda1cc18fdf35
tst <tylerstone835@gmail.com>
2026-07-06 01:08:28 -0600

    create configure_sidebar() subroutine

--- Utils/st_utils.py
+++ Utils/st_utils.py
@@ -3,6 +3,8 @@ import streamlit as st
 from pandas import DataFrame
 from pandas.io.parsers import TextFileReader
 
+from Static.company import company_dict
+
 
 @st.cache_data(scope='session')
 def get_stock_data(
@@ -49,3 +51,70 @@ def set_dashboard_style() -> None:
             """,
         unsafe_allow_html=True
     )
+
+def configure_sidebar() -> dict:
+    """
+    Draw sidebar elements and collect widget inputs into a dict struct.
+
+    :return: Dictionary containing sidebar values/data.
+    """
+
+    sidebar_dict = {}
+
+    with st.sidebar:
+        st.space('xxsmall')
+
+        st.write('# Chart Options')
+
+        sidebar_dict['symbol'] = st.selectbox(
+            label='**Company**',
+            options=[company for company in company_dict],
+            index=0
+        )
+
+        st.space('xxsmall')
+
+        sidebar_dict['timeframe'] = st.segmented_control(
+            label='**Timeframe**',
+            options=['Daily', 'Weekly'],
+            selection_mode='single',
+            default='Daily',
+            required=True
+        )
+
+        st.space('xxsmall')
+
+        sidebar_dict['style'] = st.segmented_control(
+            label='**Chart Style**',
+            options=['Candle', 'Line', 'OHLC'],
+            selection_mode='single',
+            default='Candle',
+            required=True
+        )
+
+        # Retrieve pd.DataFrame based on sidebar input
+        sidebar_dict['data'] = get_stock_data(
+            timeframe=sidebar_dict['timeframe'],
+            symbol=company_dict[sidebar_dict['symbol']]
+        )
+
+
+        st.space('xxsmall')
+
+        sidebar_dict['date_selection'] = st.select_slider(
+            label='**Date Range**',
+            options=sidebar_dict['data']['date'],
+            value=(
+                sidebar_dict['data']['date'][int(sidebar_dict['data'].index.max() / 2)],
+                sidebar_dict['data']['date'].max())
+        )
+
+        st.divider()
+        st.write('# Indicators')
+        sidebar_dict['volume'] = st.toggle(label='**Volume**', value=False)
+        sidebar_dict['macd_hist'] = st.toggle(label='**MACD Histogram**', value=True)
+        sidebar_dict['macd_lines'] = st.toggle(label='**MACD Lines**', value=False)
+        st.space('xxsmall')
+        sidebar_dict['ema'] = st.slider(label='**EMA**', min_value=0, max_value=100, value=20)
+
+        return sidebar_dict
--- main.py
+++ main.py
@@ -6,60 +6,9 @@ from Utils.st_utils import *
 
 
 set_dashboard_style()
+sb_config = configure_sidebar()
 
-# Sidebar layout
-with st.sidebar:
-    st.space('xxsmall')
-
-    st.write('# Chart Options')
-
-    symbol = st.selectbox(
-        label='**Company**',
-        options=[company for company in company_dict],
-        index=0
-    )
-
-    st.space('xxsmall')
-
-    timeframe = st.segmented_control(
-        label='**Timeframe**',
-        options=['Daily', 'Weekly'],
-        selection_mode='single',
-        default='Daily',
-        required=True
-    )
-
-    st.space('xxsmall')
-
-    style = st.segmented_control(
-        label='**Chart Style**',
-        options=['Candle', 'Line', 'OHLC'],
-        selection_mode='single',
-        default='Candle',
-        required=True
-    )
-
-    # Data Config
-    df = get_stock_data(timeframe=timeframe, symbol=company_dict[symbol])
-    bom = get_bom(df)
-
-    st.space('xxsmall')
-
-    date_selection = st.select_slider(
-        label='**Date Range**',
-        options=df['date'],
-        value=(df['date'][int(df.index.max() / 2)], df['date'].max())
-    )
-
-    st.divider()
-    st.write('# Indicators')
-    volume = st.toggle(label='**Volume**', value=False)
-    macd_hist = st.toggle(label='**MACD Histogram**', value=True)
-    macd_lines = st.toggle(label='**MACD Lines**', value=False)
-    st.space('xxsmall')
-    ema = st.slider(label='**EMA**', min_value=0, max_value=100, value=20)
-
-chart_number = 1 + volume + (macd_hist or macd_lines)
+chart_number = 1 + sb_config['volume'] + (sb_config['macd_hist'] or sb_config['macd_lines'])
 
 # Footer layout
 with st.bottom:
@@ -69,24 +18,24 @@ with st.bottom:
     )
 
 
-if not volume:
-    df.drop(columns=['volume'], inplace=True)
+if not sb_config['volume']:
+    sb_config['data'].drop(columns=['volume'], inplace=True)
 
-if not macd_hist:
-    df.drop(columns=['macd_histogram'], inplace=True)
+if not sb_config['macd_hist'] :
+    sb_config['data'].drop(columns=['macd_histogram'], inplace=True)
 
-if not macd_lines:
-    df.drop(columns=['fast_line', 'signal_line'], inplace=True)
+if not sb_config['macd_lines']:
+    sb_config['data'].drop(columns=['fast_line', 'signal_line'], inplace=True)
 
-if ema:
-    calculate_ema(df, ema)
+if sb_config['ema']:
+    calculate_ema(sb_config['data'], sb_config['ema'])
 
 # Filter date selection
-df = (
-    df
+sb_config['data'] = (
+    sb_config['data']
     .loc[
-        (df['date'] >= date_selection[0]) &
-        (df['date'] <= date_selection[1])
+        (sb_config['data']['date'] >= sb_config['date_selection'][0]) &
+        (sb_config['data']['date'] <= sb_config['date_selection'][1])
     ]
     .reset_index(drop=True)
 )
@@ -98,63 +47,64 @@ if chart_number == 1:
 
 fig.set_facecolor((0, 0, 0, 0))
 plt.tight_layout()
+bom = get_bom(sb_config['data'])
 
-if style == 'OHLC':
+if sb_config['style'] == 'OHLC':
     plot_ohlc(
         axes=ax[0],
-        df=df,
+        df=sb_config['data'],
         xticks=bom
     )
 
-elif style == 'Candle':
+elif sb_config['style'] == 'Candle':
     plot_candle(
         axes=ax[0],
-        df=df,
+        df=sb_config['data'],
         xticks=bom
     )
 
 else:
     plot_line(
         axes=ax[0],
-        df=df,
+        df=sb_config['data'],
         xticks=bom
     )
 
-if ema:
+if sb_config['ema']:
     plot_ema(
         axes=ax[0],
-        df=df,
-        window=ema,
+        df=sb_config['data'],
+        window=sb_config['ema'],
         xticks=bom
     )
 
-if volume:
+if sb_config['volume']:
     plot_volume(
         axes=ax[1],
-        df=df,
+        df=sb_config['data'],
         xticks=bom
     )
 
-if macd_hist or macd_lines:
+if sb_config['macd_hist'] or sb_config['macd_lines']:
     plot_macd(
         axes=ax[chart_number - 1],
-        df=df,
-        plot_hist=macd_hist,
-        plot_lines=macd_lines,
+        df=sb_config['data'],
+        plot_hist=sb_config['macd_hist'],
+        plot_lines=sb_config['macd_lines'],
         xticks=bom
     )
 
 # Body layout
 st.title('Fortune 5 - Price Action Analysis', text_alignment='center')
 with st.container(border=True, horizontal_alignment='center'):
-    # with st.container(horizontal_alignment='right'):
+
     with st.container(horizontal=True):
 
         with st.container(horizontal_alignment='left', vertical_alignment='top'):
             st.metric(
                 label='Close Price',
-                value=df.loc[df.index.max(), 'close'],
-                delta=calculate_close_delta(df),
+                value=sb_config['data'].loc[sb_config['data'].index.max(), 'close'],
+                delta=calculate_close_delta(sb_config['data']),
                 label_visibility='collapsed'
             )
 
@@ -171,4 +121,8 @@ with st.container(border=True, horizontal_alignment='center'):
     if viz_output == 'Chart':
         st.pyplot(fig=fig, width='content')
     elif viz_output == 'DataFrame':
-        st.dataframe(df.sort_values(by='date', ascending=False), height='content', hide_index=True)
+        st.dataframe(
+            sb_config['data'].sort_values(by='date', ascending=False),
+            height='content',
+            hide_index=True
+        )
